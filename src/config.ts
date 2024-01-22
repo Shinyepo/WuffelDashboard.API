4f70bafe3cd34bf62df122d561f844f0d50343f0
Shinyepo <69401477+Shinyepo@users.noreply.github.com>
2024-01-22 13:08:07 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 const config = {
   apiEndpoint: "https://discord.com/api",
-  botToken: "NjEzMDczNDM4OTAyNDUyMjM5.XVrnIg.T8j7Ae_yeKKDoKZzLHF_xKJIQwg",
-  clientId: "613073438902452239",
-  clientSecret: "X5fnwP1SvQXuhm0Rm2o_agfQFeFyxhB-",
+  botToken: "",
+  clientId: "",
+  clientSecret: "",
   redirectUri: "http://localhost:4000/discord/auth/callback",
   scope: ["identify", "guilds"],
   grantType: "authorization_code",
